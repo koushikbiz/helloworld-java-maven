a39dc5a568680f175a20221205a3717039cace24
koushikbiz <38094369+koushikbiz@users.noreply.github.com>
2018-04-07 09:47:29 -0700

    Delete PipFactorService.java

--- src/main/java/com/integral/ds/emscope/rates/PipFactorService.java
+++
@@ -1,8 +0,0 @@
-package com.integral.ds.emscope.rates;
-
-/**
- * Created by bhattacharjeer on 12/11/2016.
- */
-public interface PipFactorService {
-    public Long getPipFactor(String ccyPair);
-}
